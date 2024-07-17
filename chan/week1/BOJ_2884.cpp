8643ff09cf260eb6668091aeca7db2b840a0726e
geunchanKim <mouse4432@gmail.com>
2024-07-17 23:51:16 +0900

    [BOJ][2884] 알람 시계

---
+++ chan/week1/BOJ_2884.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int H, M;
+    cin >> H >> M;
+    if(M < 45){
+        if(H==0)
+            H = 23;
+        else
+            H--;
+        M = 60 - (45-M);
+    }else{
+        M -= 45;
+    }
+    cout << H << " " << M;
+}
