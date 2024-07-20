0e0e0821b37d243b63cbeba53dc67341de547da2
geunchanKim <mouse4432@gmail.com>
2024-07-20 22:57:05 +0900

    [BOJ][2675] 문자열의 반복

---
+++ chan/week1/BOJ_2675.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int T, R;
+    cin >> T;
+    for(int i = 0; i < T; i++){
+        string S;
+        cin >> R >> S;
+        for(int j = 0; j < S.length(); j++){
+            for(int k = 0; k < R; k++)
+                cout << S[j];
+        }
+        cout << "\n";
+    }
+}
