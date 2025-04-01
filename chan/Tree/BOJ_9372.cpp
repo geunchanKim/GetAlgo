daf072ad4efe43aff2dfc1b25471f5e7a7ed7b6c
geunchanKim <mouse4432@gmail.com>
2025-04-01 10:22:58 +0900

    [BOJ][9372] 상근이의 여행

---
+++ chan/Tree/BOJ_9372.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+using namespace std;
+
+int main(){
+    FASTIO;
+    int T;
+    cin >> T;
+    for(int i = 0; i < T; i++){
+        int N, M;
+        cin >> N >> M;
+        for(int j = 0; j < M; j++){
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << N - 1 << '\n';
+    }
+}
