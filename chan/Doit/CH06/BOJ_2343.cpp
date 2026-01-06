92bf013a5c6c16274f1d2d8d7b0d3e67c37d1440
geunchanKim <mouse4432@gmail.com>
2026-01-06 11:30:13 +0900

    [BOJ][2343] 기타 레슨

---
+++ chan/Doit/CH06/BOJ_2343.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+using namespace std;
+
+int main(){
+    FASTIO;
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N);
+    int start = 0;
+    int end = 0;
+    for(int i = 0; i < N; i++){
+        cin >> A[i];
+        if(start < A[i])
+            start = A[i];
+        end = end + A[i];
+    }
+    while(start <= end){
+        int middle = (start + end) / 2;
+        int sum = 0;
+        int count = 0;
+        for(int i = 0; i < N; i++){
+            if(sum + A[i] > middle){
+                count++;
+                sum = 0;
+            }
+            sum = sum + A[i];
+        }
+        if(sum != 0)
+            count++;
+        if(count > M)
+            start = middle + 1;
+        else
+            end = middle - 1;
+    }
+    cout << start;
+    return 0;
+}
